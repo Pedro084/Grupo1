f4044c20ebeff3b050ae9c12e061cbde4b60df7b
LeandroLuz19 <116946596+LeandroLuz19@users.noreply.github.com>
2022-11-15 17:30:15 +0000

    Remover comentário

--- ParticipationRoulette-master/Program.cs
+++ ParticipationRoulette-master/Program.cs
@@ -74,7 +74,7 @@
             students.Add("FRANCISCO DIAS");
             students.Add("LEONARDO Andrade");
 
-            //Get Random Value to find student
+            
             //Este é outro comentário que me tinha esquecido. 
             var random = new Random(DateTime.Now.Millisecond);
             var  index = random.Next(students.Count);
